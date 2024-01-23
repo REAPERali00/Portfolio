0badba107f7e755a06911863d1d4ff21d95f54c4
Alireza <jadi0002@algonquinlive.com>
2024-01-23 13:24:58 -0500

    Finished Portfolio Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,107 +1,114 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-const navMenu = document.getElementById('nav-menu'), 
-    navToggle = document.getElementById('nav-toggle'), 
-    navClose = document.getElementById('nav-close');
-
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', ()=>{
-        navMenu.classList.add('show-menu');
-    });
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', ()=>{
-        navMenu.classList.remove('show-menu');
-    });
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav_link');
+const navLink = document.querySelectorAll(".nav_link");
 
-function linkAction(){
-    navMenu.classList.remove('show-menu')
+function linkAction() {
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n=>n.addEventListener('click', linkAction)); 
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
-const skillsContent = document.getElementsByClassName('skills_content'), 
-      skillsHeader = document.querySelectorAll('.skills_header'); 
+const skillsContent = document.getElementsByClassName("skills_content"),
+  skillsHeader = document.querySelectorAll(".skills_header");
 
-function toggleSkills(){
-    let itemClass = this.parentNode.className; 
-    for (let i = 0; i < skillsContent.length; i++){
-        skillsContent[i].className = 'skills_content skills_close'; 
-    }
-    if(itemClass === 'skills_content skills_close'){
-        this.parentNode.className = 'skills_content skills_open';
-    } else {
-        this.parentNode.className = 'skills_content skills_close';
-    }
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+  for (let i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills_content skills_close";
+  }
+  if (itemClass === "skills_content skills_close") {
+    this.parentNode.className = "skills_content skills_open";
+  } else {
+    this.parentNode.className = "skills_content skills_close";
+  }
 }
 
 skillsHeader.forEach((el) => {
-    el.addEventListener('click', toggleSkills);
+  el.addEventListener("click", toggleSkills);
 });
 
 /*==================== QUALIFICATION TABS ====================*/
-const tabs = document.querySelectorAll("[data-target]"), 
-    tabContents = document.querySelectorAll("[data-content]"); 
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[data-content]");
 
-tabs.forEach(tab=>{
-    tab.addEventListener('click', ()=>{
-        const target = document.querySelector(tab.dataset.target); 
-        tabContents.forEach(tabContent=>{
-            tabContent.classList.remove('qual_active'); 
-        })
-        target.classList.add('qual_active'); 
-        tabs.forEach(tab=>{
-            tab.classList.remove('qual_active'); 
-        })
-        tab.classList.add('qual_active');
-    }); 
-}); 
-
-/*==================== SERVICES MODAL ====================*/
-const modalViews = document.querySelectorAll('.services_modal'), 
-    modalBtns = document.querySelectorAll('.services_button'), 
-    modalCloses = document.querySelectorAll('.services_modal-close'); 
-
-let modal = function(modalClick){
-    modalViews[modalClick].classList.add('active-modal'); 
-}
-
-modalBtns.forEach((modalBtn, i) =>{
-    modalBtn.addEventListener('click', ()=>{
-        modal(i); 
-    }); 
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("qual_active");
+    });
+    target.classList.add("qual_active");
+    tabs.forEach((tab) => {
+      tab.classList.remove("qual_active");
+    });
+    tab.classList.add("qual_active");
+  });
 });
 
-modalCloses.forEach((modalClose) =>{
-    modalClose.addEventListener('click', ()=>{
-        modalViews.forEach((modalView) =>{
-            modalView.classList.remove('active-modal');
-        })
-    }); 
-}); 
+/*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll(".services_modal"),
+  modalBtns = document.querySelectorAll(".services_button"),
+  modalCloses = document.querySelectorAll(".services_modal-close");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper(".port_container", {
+  cssMode: true,
+  loop: true,
 
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
 /*==================== TESTIMONIAL ====================*/
 
-
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
-
-/*==================== SHOW SCROLL UP ====================*/ 
-
-
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
